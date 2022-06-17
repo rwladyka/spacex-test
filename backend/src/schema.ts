dc8bfd613d19958bf1ea0622816673e6eed1dc22
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2022-06-17 04:21:06 -0300

    fix: remove console.log

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,6 +1,5 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import axios from 'axios'
-import { log } from 'console'
 import { routes } from './routes'
 import { typeDefinitions } from './typeDef'
 
@@ -8,22 +7,18 @@ const resolvers = {
   Query: {
     latest: async () => {
       const { data } = await axios.get(routes().latest)
-      log(data)
       return data
     },
     next: async () => {
       const { data } = await axios.get(routes().next)
-      log(data)
       return data
     },
     past: async () => {
       const { data } = await axios.get(routes().past)
-      log(data)
       return data
     },
     upcoming: async () => {
       const { data } = await axios.get(routes().upcoming)
-      log(data)
       return data
     },
   },
