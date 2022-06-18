7c68ead8e73e3b1434d2ce32d9ac17df934e178b
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2022-06-18 14:17:29 -0300

    test: Next component rendering

---
+++ front/src/components/Launches/Next.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import App from 'App'
+import { MockedProvider } from '@apollo/client/testing'
+import { nextMock } from 'test/launchMockData'
+import moment from 'moment'
+
+it('should render the next launch', async () => {
+  render(
+    <MockedProvider mocks={nextMock}>
+      <App />
+    </MockedProvider>,
+  )
+
+  const nextLink = await screen.findByText(/NEXT LAUNCH/i)
+  await fireEvent.click(nextLink)
+
+  await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+  const title = await screen.findByText('Globalstar FM15')
+  expect(title).toBeInTheDocument()
+
+  const flightNumberText = screen.getByText(/Número do Voo/)
+  expect(flightNumberText).toBeInTheDocument()
+
+  const flightNumberValue = screen.getByText('169')
+  expect(flightNumberValue).toBeInTheDocument()
+
+  const launchDateText = screen.getByText(/Data de lançamento/)
+  expect(launchDateText).toBeInTheDocument()
+
+  const launchDateValue = screen.getByText(
+    moment(nextMock[0].result.data.next.date_utc).format('DD/MM/yyyy'),
+  )
+  expect(launchDateValue).toBeInTheDocument()
+
+  const launchHourText = screen.getByText(/Hora de lançamento \(UTC\)/)
+  expect(launchHourText).toBeInTheDocument()
+
+  const launchHourValue = screen.getByText(
+    moment(nextMock[0].result.data.next.date_utc).format('HH:mm'),
+  )
+  expect(launchHourValue).toBeInTheDocument()
+
+  expect(() => screen.getByText(/Links/i)).toThrow()
+  expect(() => screen.getByText(/Reddit/i)).toThrow()
+  expect(() => screen.getByText(/Youtube/i)).toThrow()
+  expect(() => screen.getByText(/Wikipedia/i)).toThrow()
+  expect(() => screen.getByText(/Logo/i)).toThrow()
+  expect(() => screen.getByRole('img')).toThrow()
+})
