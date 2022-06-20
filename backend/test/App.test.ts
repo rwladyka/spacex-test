ee4fd0967ba5864b4a9437e2c0de1e5b325cac9c
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2022-06-20 16:26:41 -0300

    test: fix test with pagination

--- backend/test/App.test.ts
+++ backend/test/App.test.ts
@@ -1,6 +1,26 @@
 import { createServer } from '@graphql-yoga/common'
 import { schema } from './schemaMock'
 
+const getBody = (query: string) => {
+  if (query.match(/past|upcoming/g)) {
+    return `{ ${query} {
+      totalItems
+      launches {
+        name
+        date_utc
+        flight_number
+      }
+    }}`
+  }
+
+  return `{ ${query} {
+      name
+      date_utc
+      flight_number
+    }
+  }`
+}
+
 const get = async (query: string) => {
   const response = await createServer({
     schema,
@@ -10,11 +30,7 @@ const get = async (query: string) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      query: `{ ${query} {
-        name
-        date_utc
-        flight_number
-      }}`,
+      query: getBody(query),
     }),
   })
 
@@ -42,33 +58,40 @@ it('should request to next', async () => {
 })
 
 it('should request to past', async () => {
-  const response = await get('past')
+  const response = await get('past(limit: 10, page: 1)')
 
   const executionResult = await response.json()
 
   expect(response.status).toBe(200)
-  expect(executionResult.data.past[0].name).toBe('FalconSat')
-  expect(executionResult.data.past[0].flight_number).toBe(1)
-  expect(executionResult.data.past[0].date_utc).toBe('2006-03-24T22:30:00.000Z')
-  expect(executionResult.data.past[1].name).toBe('DemoSat')
-  expect(executionResult.data.past[1].flight_number).toBe(2)
-  expect(executionResult.data.past[1].date_utc).toBe('2007-03-21T01:10:00.000Z')
+  expect(executionResult.data.past.launches[0].name).toBe('FalconSat')
+  expect(executionResult.data.past.launches[0].flight_number).toBe(1)
+  expect(executionResult.data.past.launches[0].date_utc).toBe(
+    '2006-03-24T22:30:00.000Z',
+  )
+  expect(executionResult.data.past.launches[1].name).toBe('DemoSat')
+  expect(executionResult.data.past.launches[1].flight_number).toBe(2)
+  expect(executionResult.data.past.launches[1].date_utc).toBe(
+    '2007-03-21T01:10:00.000Z',
+  )
 })
 
 it('should request to upcoming', async () => {
-  const response = await get('upcoming')
+  const response = await get('upcoming(limit: 10, page: 1)')
+  console.log(response)
 
   const executionResult = await response.json()
-
+  console.log(executionResult)
   expect(response.status).toBe(200)
-  expect(executionResult.data.upcoming[0].name).toBe('Globalstar FM15')
-  expect(executionResult.data.upcoming[0].flight_number).toBe(169)
-  expect(executionResult.data.upcoming[0].date_utc).toBe(
+  expect(executionResult.data.upcoming.launches[0].name).toBe('Globalstar FM15')
+  expect(executionResult.data.upcoming.launches[0].flight_number).toBe(169)
+  expect(executionResult.data.upcoming.launches[0].date_utc).toBe(
     '2022-06-19T04:27:00.000Z',
   )
-  expect(executionResult.data.upcoming[1].name).toBe('Starlink 4-21 (v1.5)')
-  expect(executionResult.data.upcoming[1].flight_number).toBe(170)
-  expect(executionResult.data.upcoming[1].date_utc).toBe(
+  expect(executionResult.data.upcoming.launches[1].name).toBe(
+    'Starlink 4-21 (v1.5)',
+  )
+  expect(executionResult.data.upcoming.launches[1].flight_number).toBe(170)
+  expect(executionResult.data.upcoming.launches[1].date_utc).toBe(
     '2022-06-26T00:43:00.000Z',
   )
 })
--- backend/test/schemaMock.ts
+++ backend/test/schemaMock.ts
@@ -38,78 +38,84 @@ const resolvers = {
         },
       },
     }),
-    past: () => [
-      {
-        launchpad: '5e9e4502f5090995de566f86',
-        name: 'FalconSat',
-        date_utc: '2006-03-24T22:30:00.000Z',
-        flight_number: 1,
-        links: {
-          webcast: 'https://www.youtube.com/watch?v=0a_00nJ_Y88',
-          wikipedia: 'https://en.wikipedia.org/wiki/DemoSat',
-          youtube_id: '0a_00nJ_Y88',
-          reddit: {
-            launch: null,
-          },
-          patch: {
-            small: 'https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png',
-          },
-        },
-      },
-      {
-        launchpad: '5e9e4502f5090995de566f86',
-        name: 'DemoSat',
-        date_utc: '2007-03-21T01:10:00.000Z',
-        flight_number: 2,
-        links: {
-          webcast: 'https://www.youtube.com/watch?v=Lk4zQ2wP-Nc',
-          wikipedia: 'https://en.wikipedia.org/wiki/DemoSat',
-          youtube_id: 'Lk4zQ2wP-Nc',
-          reddit: {
-            launch: null,
-          },
-          patch: {
-            small: 'https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png',
+    past: () => ({
+      totalItems: 2,
+      launches: [
+        {
+          launchpad: '5e9e4502f5090995de566f86',
+          name: 'FalconSat',
+          date_utc: '2006-03-24T22:30:00.000Z',
+          flight_number: 1,
+          links: {
+            webcast: 'https://www.youtube.com/watch?v=0a_00nJ_Y88',
+            wikipedia: 'https://en.wikipedia.org/wiki/DemoSat',
+            youtube_id: '0a_00nJ_Y88',
+            reddit: {
+              launch: null,
+            },
+            patch: {
+              small: 'https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png',
+            },
           },
         },
-      },
-    ],
-    upcoming: () => [
-      {
-        launchpad: '5e9e4501f509094ba4566f84',
-        name: 'Globalstar FM15',
-        date_utc: '2022-06-19T04:27:00.000Z',
-        flight_number: 169,
-        links: {
-          webcast: null,
-          wikipedia: null,
-          youtube_id: null,
-          reddit: {
-            launch: null,
-          },
-          patch: {
-            small: null,
+        {
+          launchpad: '5e9e4502f5090995de566f86',
+          name: 'DemoSat',
+          date_utc: '2007-03-21T01:10:00.000Z',
+          flight_number: 2,
+          links: {
+            webcast: 'https://www.youtube.com/watch?v=Lk4zQ2wP-Nc',
+            wikipedia: 'https://en.wikipedia.org/wiki/DemoSat',
+            youtube_id: 'Lk4zQ2wP-Nc',
+            reddit: {
+              launch: null,
+            },
+            patch: {
+              small: 'https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png',
+            },
           },
         },
-      },
-      {
-        launchpad: '5e9e4501f509094ba4566f84',
-        name: 'Starlink 4-21 (v1.5)',
-        date_utc: '2022-06-26T00:43:00.000Z',
-        flight_number: 170,
-        links: {
-          webcast: null,
-          wikipedia: null,
-          youtube_id: null,
-          reddit: {
-            launch: null,
-          },
-          patch: {
-            small: 'https://imgur.com/BrW201S.png',
+      ],
+    }),
+    upcoming: () => ({
+      totalItems: 2,
+      launches: [
+        {
+          launchpad: '5e9e4501f509094ba4566f84',
+          name: 'Globalstar FM15',
+          date_utc: '2022-06-19T04:27:00.000Z',
+          flight_number: 169,
+          links: {
+            webcast: null,
+            wikipedia: null,
+            youtube_id: null,
+            reddit: {
+              launch: null,
+            },
+            patch: {
+              small: null,
+            },
           },
         },
-      },
-    ],
+        {
+          launchpad: '5e9e4501f509094ba4566f84',
+          name: 'Starlink 4-21 (v1.5)',
+          date_utc: '2022-06-26T00:43:00.000Z',
+          flight_number: 170,
+          links: {
+            webcast: null,
+            wikipedia: null,
+            youtube_id: null,
+            reddit: {
+              launch: null,
+            },
+            patch: {
+              small: 'https://imgur.com/BrW201S.png',
+            },
+          },
+        },
+      ],
+    }),
   },
 }
 
