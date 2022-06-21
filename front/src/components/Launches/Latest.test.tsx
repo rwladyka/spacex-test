75e0ad5777f8d0667a8a0915eda720eb8504fef6
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2022-06-21 10:24:38 -0300

    refactory: added describe to tests

--- front/src/components/Launches/Latest.test.tsx
+++ front/src/components/Launches/Latest.test.tsx
@@ -5,53 +5,55 @@ import { MockedProvider } from '@apollo/client/testing'
 import { latestMock } from 'test/launchMockData'
 import moment from 'moment'
 
-it('should render the latest launch', async () => {
-  render(
-    <MockedProvider mocks={latestMock}>
-      <Latest />
-    </MockedProvider>,
-  )
+describe('Latest Component', () => {
+  it('should render the latest launch', async () => {
+    render(
+      <MockedProvider mocks={latestMock}>
+        <Latest />
+      </MockedProvider>,
+    )
 
-  await waitFor(() => new Promise((res) => setTimeout(res, 0)))
-  const title = await screen.findByText('Starlink 4-19 (v1.5)')
-  expect(title).toBeInTheDocument()
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    const title = await screen.findByText('Starlink 4-19 (v1.5)')
+    expect(title).toBeInTheDocument()
 
-  const img = await screen.findAllByRole('img')
-  expect(img.length).toBe(2)
+    const img = await screen.findAllByRole('img')
+    expect(img.length).toBe(2)
 
-  const flightNumberText = screen.getByText(/Número do Voo/)
-  expect(flightNumberText).toBeInTheDocument()
+    const flightNumberText = screen.getByText(/Número do Voo/)
+    expect(flightNumberText).toBeInTheDocument()
 
-  const flightNumberValue = screen.getByText('167')
-  expect(flightNumberValue).toBeInTheDocument()
+    const flightNumberValue = screen.getByText('167')
+    expect(flightNumberValue).toBeInTheDocument()
 
-  const launchDateText = screen.getByText(/Data de lançamento/)
-  expect(launchDateText).toBeInTheDocument()
+    const launchDateText = screen.getByText(/Data de lançamento/)
+    expect(launchDateText).toBeInTheDocument()
 
-  const launchDateValue = screen.getByText(
-    moment(latestMock[0].result.data.latest.date_utc).format('DD/MM/yyyy'),
-  )
-  expect(launchDateValue).toBeInTheDocument()
+    const launchDateValue = screen.getByText(
+      moment(latestMock[0].result.data.latest.date_utc).format('DD/MM/yyyy'),
+    )
+    expect(launchDateValue).toBeInTheDocument()
 
-  const launchHourText = screen.getByText(/Hora de lançamento \(UTC\)/)
-  expect(launchHourText).toBeInTheDocument()
+    const launchHourText = screen.getByText(/Hora de lançamento \(UTC\)/)
+    expect(launchHourText).toBeInTheDocument()
 
-  const launchHourValue = screen.getByText(
-    moment(latestMock[0].result.data.latest.date_utc).format('HH:mm'),
-  )
-  expect(launchHourValue).toBeInTheDocument()
+    const launchHourValue = screen.getByText(
+      moment(latestMock[0].result.data.latest.date_utc).format('HH:mm'),
+    )
+    expect(launchHourValue).toBeInTheDocument()
 
-  const linksTitle = screen.getByText(/Links/)
-  expect(linksTitle).toBeInTheDocument()
+    const linksTitle = screen.getByText(/Links/)
+    expect(linksTitle).toBeInTheDocument()
 
-  const reddit = screen.getByText(/Reddit/i)
-  expect(reddit).toBeInTheDocument()
+    const reddit = screen.getByText(/Reddit/i)
+    expect(reddit).toBeInTheDocument()
 
-  const youtube = screen.getByText(/Youtube/i)
-  expect(youtube).toBeInTheDocument()
+    const youtube = screen.getByText(/Youtube/i)
+    expect(youtube).toBeInTheDocument()
 
-  expect(() => screen.getByText(/Wikipedia/i)).toThrow()
+    expect(() => screen.getByText(/Wikipedia/i)).toThrow()
 
-  const logoTitle = screen.getByText(/Logo/)
-  expect(logoTitle).toBeInTheDocument()
+    const logoTitle = screen.getByText(/Logo/)
+    expect(logoTitle).toBeInTheDocument()
+  })
 })
